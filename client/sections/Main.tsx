e30a3edbda395aec916a86415454a7d845fedf7b
jungboixitinh <halehoangbao@gmail.com>
2023-06-01 21:50:57 +0700

    Update Main.tsx

--- client/sections/Main.tsx
+++ client/sections/Main.tsx
@@ -4,6 +4,7 @@ const Main = () => {
     return(
         <>
         XuanBao 01 - t moi them o branch cua t vao day
+                   - t moi thi xong gio moi vao xem 
         hihihihi
         </>
     )
