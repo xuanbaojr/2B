77aaf8d7ce6abb1f999d24f0b77f4acdcf682836
jungboixitinh <halehoangbao@gmail.com>
2023-06-25 18:02:46 +0700

    help
    hihi

--- client/sections/Main.tsx
+++ client/sections/Main.tsx
@@ -6,6 +6,7 @@ const Main = () => {
         XuanBao 01 - t moi them o branch cua t vao day
                    - t moi thi xong gio moi vao xem 
         hihihihi
+        25 - 06 test
         </>
     )
 }
