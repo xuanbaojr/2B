0831bddff322388909e1311a7d372cdee99671d1
jungboixitinh <phanxuanbaoa0@gmail.com>
2023-06-29 16:06:47 +0700

    29/06

--- client/sections/Main.tsx
+++ client/sections/Main.tsx
@@ -3,10 +3,7 @@
 const Main = () => {
     return(
         <>
-        XuanBao 01 - t moi them o branch cua t vao day
-                   - t moi thi xong gio moi vao xem 
-        hihihihi
-        25 - 06 test
+        29/06 test 
         </>
     )
 }
